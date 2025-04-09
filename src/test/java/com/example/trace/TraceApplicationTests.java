c1d3889f3be3a3bb009d0fd9bfd614e5df14f510
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-04-09 09:16:52 +0900

    Initial commit

---
+++ src/test/java/com/example/trace/TraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
