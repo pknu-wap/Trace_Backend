105128754509dc3c2fdc1446d680800d10c0a1c6
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-04 17:16:10 +0900

    #33 [CHORE] swagger 및 spring docs 설정

--- src/main/java/com/example/trace/auth/dto/TokenResponse.java
+++ src/main/java/com/example/trace/auth/dto/TokenResponse.java
@@ -1,13 +1,16 @@
 package com.example.trace.auth.dto;
 
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@Schema(description = "토큰 응답 DTO")
 public class TokenResponse {
+    @Schema(description = "JWT 액세스 토큰", example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicm9sZSI6IlJPTEVfVVNFUiIsI...")
     private final String accessToken;
+    
+    @Schema(description = "JWT 리프레시 토큰", example = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwicm9sZSI6IlJPTEVfVVNFUiIsI...")
     private final String refreshToken;
-    // Lombok의 @Builder 사용시 모든 필드가 필요하므로 명시적 생성자 추가
 }
