51a993e09368da08ddebfa648626571d2c85c539
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-05 10:28:28 +0900

    #33 [CHORE] spring docs 라이브러리로 swagger ui 구성

---
+++ src/main/java/com/example/trace/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.example.trace.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("흔적 API")
+                        .description("흔적 애플리케이션 API 문서")
+                        .version(appVersion)
+                        .contact(new Contact()
+                                .name("흔적")
+                                .url("https://github.com/pknu-wap/Trace_Backend")
+                                .email("wjsbdcindsu@pukyong.ac.kr"))
+                        .license(new License()
+                                .name("Apache License Version 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("JWT 토큰을 입력해주세요.")));
+    }
+}
