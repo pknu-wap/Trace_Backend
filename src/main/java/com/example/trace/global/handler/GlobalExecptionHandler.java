f2648e65953ebe4520aa3f307c0f4deeccd31a33
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-11 22:58:16 +0900

    #47 [REFACTOR] 전역 예외 처리 적용

--- src/main/java/com/example/trace/global/handler/GlobalExecptionHandler.java
+++ src/main/java/com/example/trace/global/handler/GlobalExecptionHandler.java
@@ -1,7 +1,13 @@
 package com.example.trace.global.handler;
 
+import com.example.trace.global.errorcode.AuthErrorCode;
+import com.example.trace.global.errorcode.ErrorCode;
+import com.example.trace.global.errorcode.SignUpErrorCode;
 import com.example.trace.global.errorcode.TokenErrorCode;
+import com.example.trace.global.execption.AuthExecption;
+import com.example.trace.global.execption.SignUpExecption;
 import com.example.trace.global.execption.TokenExecption;
+import com.example.trace.global.response.ErrorResponse;
 import com.example.trace.global.response.TokenErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,13 +17,38 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class GlobalExecptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(TokenExecption.class)
-    public ResponseEntity<Object> handleCustomException(TokenExecption e) {
+    public ResponseEntity<Object> handleTokenException(TokenExecption e) {
         TokenErrorCode tokenErrorCode = e.getTokenErrorCode();
         return handleExceptionInternal(tokenErrorCode);
     }
+
+    @ExceptionHandler(AuthExecption.class)
+    public ResponseEntity<Object> handleAuthException(AuthExecption e) {
+        AuthErrorCode authErrorCode = e.getAuthErrorCode();
+        return handleExceptionInternal(authErrorCode);
+    }
+
+    @ExceptionHandler(SignUpExecption.class)
+    public ResponseEntity<Object> handleSignUpException(SignUpExecption e) {
+        SignUpErrorCode signUpErrorCode = e.getSignUpErrorCode();
+        return handleExceptionInternal(signUpErrorCode);
+    }
+
+
+
     private ResponseEntity<Object> handleExceptionInternal(TokenErrorCode tokenErrorCode) {
         return ResponseEntity.status(tokenErrorCode.getHttpStatus())
                 .body(makeTokenErrorResponse(tokenErrorCode));
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(AuthErrorCode authErrorCode) {
+        return ResponseEntity.status(authErrorCode.getHttpStatus())
+                .body(makeErrorResponse(authErrorCode));
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(SignUpErrorCode signUpErrorCode) {
+        return ResponseEntity.status(signUpErrorCode.getHttpStatus())
+                .body(makeErrorResponse(signUpErrorCode));
     }
     private TokenErrorResponse makeTokenErrorResponse(TokenErrorCode tokenErrorCode) {
         return TokenErrorResponse.builder()
@@ -27,4 +58,11 @@ public class GlobalExecptionHandler extends ResponseEntityExceptionHandler {
                 .isValid(tokenErrorCode.isValid())
                 .build();
     }
+    private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .build();
+    }
+
 }
