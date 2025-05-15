fea3f614354efbd5bd711164760ec14611e86545
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-16 00:06:04 +0900

    #57 [FIX] jsonpropery 적용 및 필드 구조 개선

--- src/main/java/com/example/trace/post/dto/comment/CommentDto.java
+++ src/main/java/com/example/trace/post/dto/comment/CommentDto.java
@@ -1,6 +1,7 @@
 package com.example.trace.post.dto.comment;
 
 import com.example.trace.post.domain.Comment;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,7 +9,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,26 +16,36 @@ import java.util.List;
 @Data
 @Schema(description = "댓글 DTO")
 public class CommentDto {
+
     @Schema(description = "게시글 ID", example = "1")
     private Long postId;
+
     @Schema(description = "댓글 쓴 사람의 providerID", example = "431256341")
     private String providerId;
+
     @Schema(description = "댓글 ID", example = "1")
     private Long commentId;
+
     @Schema(description = "부모 댓글 ID", example = "1")
     private Long parentId;
+
     @Schema(description = "삭제 여부", example = "false")
+    @JsonProperty("isDeleted")
     private boolean isDeleted;
+
     @Schema(description = "댓글 소유 여부", example = "true")
+    @JsonProperty("isOwner")
     private boolean isOwner;
+
     @Schema(description = "댓글 작성자 닉네임", example = "닉네임")
     private String nickName;
+
     @Schema(description = "댓글 작성자 프로필 이미지 URL", example = "https://example.com/profile.jpg")
     private String userProfileImageUrl;
+
     @Schema(description = "댓글 내용", example = "댓글 내용")
     private String content;
-    @Schema(description = "대댓글 리스트")
-    private List<Comment> childrenComments;
+
     @Schema(description = "댓글 작성 시간", example = "2023-10-01T12:00:00")
     private LocalDateTime createdAt;
 
