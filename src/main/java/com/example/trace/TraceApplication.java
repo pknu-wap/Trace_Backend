7cd4e959165a35b5a984d6825003b4c1ac5290f6
yw6938 <babybuddy6938@gmail.com>
2025-05-20 20:32:23 +0900

    Revert "#48 [FEATURE] 미션 중복할당 변경"
    This reverts commit d25737bb28143700b7a46a3449952364875eb6ab.

--- src/main/java/com/example/trace/TraceApplication.java
+++ src/main/java/com/example/trace/TraceApplication.java
@@ -3,9 +3,7 @@ package com.example.trace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 @EnableFeignClients
 public class TraceApplication {
