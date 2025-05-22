4e37eb455b8595d0069bd0c291f182b4e09d4600
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-22 17:37:20 +0900

    #71 [FEATURE] 전체 게시글 페이징 조회 DTO 및 커서 DTO 생성

---
+++ src/main/java/com/example/trace/post/dto/cursor/PostCursorRequest.java
@@ -0,0 +1,24 @@
+package com.example.trace.post.dto.cursor;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "게시글 커서 요청 DTO")
+public class PostCursorRequest {
+    @Schema(description = "커서 날짜 및 시간(첫 요청일 시, null)", example = "2025-05-22T08:26:36.025Z")
+    private LocalDateTime cursorDateTime;
+    @Schema(description = "커서 ID(첫 요청일 시, null)", example = "71")
+    private Long cursorId;
+    @Schema(description = "페이지 크기", example = "10")
+    private Integer size = 10; // 기본 페이지 크기
+    @Schema(description = "게시글 타입", example = "MISSION")
+    private String postType; // 필터링을 위한 게시글 타입 (선택적)
+}
