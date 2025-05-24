147aee5fe6cfa4e8a31694d73cd9f98bd52e1df9
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-24 18:23:56 +0900

    Reapply "feat : 커서 기반 댓글 페이징 조회"
    This reverts commit 8b97eb40a73e76d87b459d42e15dc9d5778174c1.

--- src/main/java/com/example/trace/post/dto/cursor/CommentCursorRequest.java
+++ src/main/java/com/example/trace/post/dto/cursor/CommentCursorRequest.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
@@ -12,13 +13,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Schema(description = "게시글 커서 요청 DTO")
-public class PostCursorRequest {
+public class CommentCursorRequest {
     @Schema(description = "커서 날짜 및 시간(첫 요청일 시, null)", example = "null")
     private LocalDateTime cursorDateTime;
+
     @Schema(description = "커서 ID(첫 요청일 시, null)", example = "null")
     private Long cursorId;
+
     @Schema(description = "페이지 크기", example = "10")
     private Integer size = 10; // 기본 페이지 크기
-    @Schema(description = "게시글 타입", example = "MISSION")
-    private String postType; // 필터링을 위한 게시글 타입 (선택적)
 }
