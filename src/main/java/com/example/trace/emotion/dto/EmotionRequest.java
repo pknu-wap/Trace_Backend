ec1e9c8c0e5b7927bd4ef09e9bdccb47ab056a13
wjsbedcindsu <wjsbdcindsu@pukyong.ac.kr>
2025-05-25 12:09:52 +0900

    #79 [REFACTOR] string 타입에서 emotionType 타입으로 요청 받도록 수정 + path로 postId 받기

--- src/main/java/com/example/trace/emotion/dto/EmotionRequest.java
+++ src/main/java/com/example/trace/emotion/dto/EmotionRequest.java
@@ -1,5 +1,6 @@
 package com.example.trace.emotion.dto;
 
+import com.example.trace.emotion.EmotionType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @Schema(description = "감정 표현 요청")
 public class EmotionRequest {
-    @Schema(description = "게시물 ID", example = "1")
-    private Long postId;
-    @Schema(description = "감정 표현 타입", example = "HEARTWARMING, GRATEFUL, IMPRESSIVE, TOUCHING, LIKABLE")
-    private String emotionType;
+    @Schema(description = "감정 표현 타입",
+            example = "HEARTWARMING",
+            allowableValues = {"HEARTWARMING", "GRATEFUL", "IMPRESSIVE", "TOUCHING", "LIKEABLE"})
+    private EmotionType emotionType;
 }
